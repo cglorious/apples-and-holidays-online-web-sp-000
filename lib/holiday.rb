eea056025bd54fff13c963ef4ad0e83690c49133
Catherine Gloria <catherinegloria@gmail.com>
2020-09-19 14:50:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,5 +79,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each do |k, v|
+    v.each do |k_two, v_two|
+      holiday_names = []
+      if v_two.include?("BBQ")
+        holiday_names << k_two
+      end
+    end
+  end
 end
